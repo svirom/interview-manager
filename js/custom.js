ae455aeff98fc812bf68ba648dee84c487a823f6
svirom <svirom@yahoo.com>
2020-08-17 10:01:45 +0300

    final fixes

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	$(document).on("click", ".video-button", function() {
 		var $video = $(this).closest('.video-section').find('.video-wrapper iframe');
 		var src = $video.attr('src');
-		var videoWrapper = $(this).closest('.intro-video').find('.video-bkgr');
+		var videoWrapper = $(this).closest('.video-section').find('.video-bkgr');
 		var buttonWrapper = $(this).closest('.button-wrapper');
 
 		$video.attr('src', src + '&autoplay=1');
